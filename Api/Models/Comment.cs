6de547fdc26a870b0c086f8609d1838948ffe4c0
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-13 20:00:02 +0200

    Initial commits for Models

--- Api/Models/Comment.cs
+++ Api/Models/Comment.cs
@@ -1,4 +1,17 @@
 ﻿namespace Api.Models
 {
-    public class Comment { }
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; }
+
+        public Stock? Stock { get; set; }
+    }
 }
--- Api/Models/Stock.cs
+++ Api/Models/Stock.cs
@@ -19,5 +19,7 @@
         public string Industry { get; set; } = string.Empty;
 
         public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
