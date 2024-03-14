54f82835cb4bc80fcb4b0c5cfc39af1f4555d61d
Florescu Alexandru <florescu.alex579774@gmail.com>
2024-03-14 11:03:49 +0200

    Repository PAttern -> Implemented interface

--- Api/Interfaces/IStockRepository.cs
+++ Api/Interfaces/IStockRepository.cs
@@ -1,10 +1,18 @@
 ﻿namespace Api.Interfaces
 {
+    using Api.DTOs.Stock;
     using Api.Models;
 
     public interface IStockRepository
     {
         Task<List<Stock>> GetAllAsync();
 
+        Task<Stock?> GetByIdAsync(int id);
+
+        Task<Stock> CreateAsync(Stock stockModel);
+
+        Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateStockRequestDto);
+
+        Task<Stock?> DeletAsync(int id);
     }
 }
--- Api/Repository/StockRepository.cs
+++ Api/Repository/StockRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Api.Data;
+using Api.DTOs.Stock;
 using Api.Interfaces;
 using Api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -12,7 +13,27 @@ namespace Api.Repository
         public StockRepository(ApplicationDbContext context)
         => this._context = context;
 
+        public Task<Stock> CreateAsync(Stock stockModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> DeletAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public Task<List<Stock>> GetAllAsync()
         => this._context.Stocks.ToListAsync();
+
+        public Task<Stock?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto updateStockRequestDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
